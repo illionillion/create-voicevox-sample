02c621fd2e348e0db0655a50141050b59b35fa9b
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-25 14:50:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# VOICEVOX音声生成サンプル
+
+以下のAPIを使用して作成
+
+https://voicevox.su-shiki.com/su-shikiapis/
+
+# 実行方法
+
+ルート直下に`.env`ファイルを作成し、以下の内容を記述
+
+```env
+VITE_API_KEY=APIキー（自分で発行したもの）
+```
+
+```sh
+npm i
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Button, Center, Container, Heading, Textarea, useBoolean, useNotice, VStack } from "@yamada-ui/react"
+import { useState } from "react"
+
+function App() {
+
+  const [inputText, setInputText] = useState<string>("")
+  const [audioData, setAudioData] = useState<Blob>()
+  const [isLoading, {on, off} ] = useBoolean()
+
+  const notice = useNotice()
+
+  const handleCreate = async () => {
+    if(!inputText) return
+    setAudioData(undefined)
+    on()
+    const apiResponse = await fetch(`https://api.tts.quest/v3/voicevox/synthesis?text=${inputText}&speaker=3&key=${import.meta.env.VITE_API_KEY}`)
+    const apiData = await apiResponse.json()
+    
+    const { mp3DownloadUrl, audioStatusUrl } = apiData
+
+    while (true) {
+      await new Promise<void>((resolve) => setTimeout(() => {
+        resolve()
+      }, 1000))
+
+      const statusResponse = await fetch(audioStatusUrl)
+      const statusJson = await statusResponse.json()
+      
+      if(statusJson.isAudioReady) break
+      if(statusJson.isAudioError) {
+        notice({
+          title: "失敗",
+          colorScheme: "danger",
+          variant: "solid"
+        })
+        off()
+        return
+      }
+    }
+    
+    const audioResponse = await fetch(mp3DownloadUrl)
+    const audioBlob = await audioResponse.blob()
+    setAudioData(audioBlob)
+    off()
+  }
+
+  return (
+    <Container mx="auto">
+      <Center flexDir="column">
+        <Heading>VOICEVOX 音声生成 サンプル</Heading>
+        <VStack>
+          <Textarea value={inputText} onChange={e => setInputText(e.currentTarget.value)} />
+          <Button onClick={handleCreate} isDisabled={isLoading}>生成</Button>
+          {
+            audioData ? <audio src={audioData ? window.URL.createObjectURL(audioData) : undefined} controls></audio> : undefined
+          }
+        </VStack>
+      </Center>
+    </Container>
+  )
+}
+
+export default App
